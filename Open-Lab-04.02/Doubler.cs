ac7ea1008b6da357901a8c51943e878a8c995dc6
tmichalikova231@gmail.com <tmichalikova231@gmail.com>
2020-10-26 14:03:57 +0100

    Done

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            string str = "";
+            for (var a = 0; a < original.Length; a++)
+            {
+
+                for (var v = 0; v < 2; v++)
+                {
+                    str = str + original[a];
+                }
+
+            }
+            return str;
         }
     }
 }
